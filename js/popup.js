5bcc1820abe32eb7591c997f93e0d8784487b06a
yasudanaoya <tamago.708@gmail.com>
2021-10-17 22:58:46 +0900

    initial commit

---
+++ js/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('check');
+  button.addEventListener('click', function() {
+    alert("Hey your button is working!");
+  }, false);
+}, false);
