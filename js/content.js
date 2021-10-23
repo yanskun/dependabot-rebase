5f18fc908491a586db1092b5538fabe69611adf8
yasudanaoya <tamago.708@gmail.com>
2021-10-24 03:35:35 +0900

    fix: manifest

--- js/content.js
+++ js/content.js
@@ -6,8 +6,17 @@ if (isDependabot) {
   const leftBtnWrapper = document.querySelector('.color-bg-subtle.ml-1')
 
   const dependabotBtn = document.createElement('button')
-  dependabotBtn.className = 'btn-primary btn'
-  dependabotBtn.style.backgroundColor = 'rgb(18, 102, 214)'
+  dependabotBtn.className = 'btn-primary btn dependabot-rebase-btn'
+  const css = ".dependabot-rebase-btn { background-color: rgb(18, 102, 214); } .dependabot-rebase-btn:hover { background-color: rgb(0, 75, 190); }"
+
+  var style = document.createElement('style');
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+  document.getElementsByTagName('head')[0].appendChild(style);
+
   dependabotBtn.textContent = `dependabot\nrebase`
   dependabotBtn.onclick = function () {
     textarea.value = '@dependabot rebase'
