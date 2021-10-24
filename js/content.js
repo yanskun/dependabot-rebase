93a0c44baf162fd59da1cd6cdd85c8a135881331
yasudanaoya <tamago.708@gmail.com>
2021-10-24 16:26:08 +0900

    fix: manifest version updated to 3

--- js/content.js
+++ js/content.js
@@ -1,10 +1,18 @@
+// branch の名前を取得しています。
+// [1]なのは、 main <- dependabot~~~　となっているので、
+// 1つ目が base branch
+// 2つ目が ブランチ名
 const branchName = document.querySelectorAll('.gh-header-meta .css-truncate span.css-truncate-target')[1].textContent
 const isDependabot = branchName.includes('dependabot')
 
+// dependabot　からの PR だったら実行する
 if (isDependabot) {
+  // コメントを入力するセレクタ
   const textarea = document.getElementById('new_comment_field')
+  // [Comment]ボタンが存在するセレクタ、今回はここに新しいボタンを追加します。
   const leftBtnWrapper = document.querySelector('.color-bg-subtle.ml-1')
 
+  // 実際に追加するボタンセレクタ
   const dependabotBtn = document.createElement('button')
   dependabotBtn.className = 'btn-primary btn dependabot-rebase-btn'
   const css = ".dependabot-rebase-btn { background-color: rgb(18, 102, 214); } .dependabot-rebase-btn:hover { background-color: rgb(0, 75, 190); }"
@@ -17,10 +25,12 @@ if (isDependabot) {
   }
   document.getElementsByTagName('head')[0].appendChild(style);
 
-  dependabotBtn.textContent = `dependabot\nrebase`
+  dependabotBtn.textContent = 'Rebase'
+  // ボタンをクリックした時になにをするのか
   dependabotBtn.onclick = function () {
     textarea.value = '@dependabot rebase'
   }
 
+  // 最後にボタンを画面に追加
   leftBtnWrapper.appendChild(dependabotBtn)
 }
