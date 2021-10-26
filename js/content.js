8cda52a130dc2f13cc82b54fa73bc88beef19231
yasudanaoya <tamago.708@gmail.com>
2021-10-26 11:20:47 +0900

    fix: syntax

--- js/content.js
+++ js/content.js
@@ -5,7 +5,7 @@
 const branchName = document.querySelectorAll('.gh-header-meta .css-truncate span.css-truncate-target')[1].textContent
 const isDependabot = branchName.includes('dependabot')
 
-// dependabot　からの PR だったら実行する
+// dependabot からの PR だったら実行する
 if (isDependabot) {
   // コメントを入力するセレクタ
   const textarea = document.getElementById('new_comment_field')
@@ -17,13 +17,13 @@ if (isDependabot) {
   dependabotBtn.className = 'btn-primary btn dependabot-rebase-btn'
   const css = ".dependabot-rebase-btn { background-color: rgb(18, 102, 214); } .dependabot-rebase-btn:hover { background-color: rgb(0, 75, 190); }"
 
-  var style = document.createElement('style');
+  var style = document.createElement('style')
   if (style.styleSheet) {
-    style.styleSheet.cssText = css;
+    style.styleSheet.cssText = css
   } else {
-    style.appendChild(document.createTextNode(css));
+    style.appendChild(document.createTextNode(css))
   }
-  document.getElementsByTagName('head')[0].appendChild(style);
+  document.getElementsByTagName('head')[0].appendChild(style)
 
   dependabotBtn.textContent = 'Rebase'
   // ボタンをクリックした時になにをするのか
